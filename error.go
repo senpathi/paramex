927e448e478a556a6c72710827767e9fc3f2cdc9
nipunapathirana <nsspathirana@gmail.com>
2021-05-04 22:03:06 +0530

    error type changed interface to structs

--- error.go
+++ error.go
@@ -1,6 +1,25 @@
 package paramex
 
-type ErrorUnSupportedParamType error
-type ErrorUnmarshalType error
-type ErrorNotAssignable error
-type ErrorUnSupportedType error
+type ErrorUnSupportedParamType struct{ err error }
+
+func (e ErrorUnSupportedParamType) Error() string {
+	return e.err.Error()
+}
+
+type ErrorUnmarshalType struct{ err error }
+
+func (e ErrorUnmarshalType) Error() string {
+	return e.err.Error()
+}
+
+type ErrorNotAssignable struct{ err error }
+
+func (e ErrorNotAssignable) Error() string {
+	return e.err.Error()
+}
+
+type ErrorUnSupportedType struct{ err error }
+
+func (e ErrorUnSupportedType) Error() string {
+	return e.err.Error()
+}
