309e552a5f4dd0bdeb70123c10db9ba1460b23d6
nipunapathirana <nsspathirana@gmail.com>
2021-05-04 22:04:07 +0530

    tests added for empty tags and some errors

--- param_extractor_test.go
+++ param_extractor_test.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
 )
 
-func TestParamExtractor_ExtractQuery(t *testing.T) {
+func makeRequest() (*http.Request, error) {
 	params :=
 		"name=" + url.QueryEscape(`query_name`) + "&" +
 			"age=" + url.QueryEscape(`20`) + "&" +
@@ -25,7 +26,7 @@ func TestParamExtractor_ExtractQuery(t *testing.T) {
 
 	req, err := http.NewRequest(`POST`, path, strings.NewReader(reqForm.Encode()))
 	if err != nil {
-		t.Error(`error creating request`, err)
+		return nil, err
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("Content-Length", strconv.Itoa(len(reqForm.Encode())))
@@ -34,6 +35,15 @@ func TestParamExtractor_ExtractQuery(t *testing.T) {
 	req.Header.Set(`age`, `40`)
 	req.Header.Set(`height`, `1.74`)
 	req.Header.Set(`married`, `true`)
+
+	return req, nil
+}
+
+func TestParamExtractor_ExtractQuery(t *testing.T) {
+	req, err := makeRequest()
+	if err != nil {
+		t.Error(`error creating request`, err)
+	}
 
 	headers := headerParams{}
 	forms := formParams{}
@@ -61,6 +71,163 @@ func TestParamExtractor_ExtractQuery(t *testing.T) {
 	fmt.Println(fmt.Sprintf(`request queries := %v`, queries))
 }
 
+func TestExtractor_ExtractHeaders_EmptyFieldTag(t *testing.T) {
+	req, err := makeRequest()
+	if err != nil {
+		t.Error(`error creating request`, err)
+	}
+
+	empty := emptyTag{}
+	extractor := NewParamExtractor()
+	err = extractor.ExtractHeaders(&empty, req)
+	if err != nil {
+		t.Errorf(`error extracting headers due to %v`, err)
+	}
+	if empty.Name != `` {
+		t.Errorf(`expected "", but received %v`, empty.Name)
+	}
+	if empty.Age != 0 {
+		t.Errorf(`expected 0, but received %v`, empty.Age)
+	}
+	if empty.Height != 0 {
+		t.Errorf(`expected 0, but received %v`, empty.Height)
+	}
+}
+
+func TestExtractor_ExtractForms_EmptyFieldTag(t *testing.T) {
+	req, err := makeRequest()
+	if err != nil {
+		t.Error(`error creating request`, err)
+	}
+
+	empty := emptyTag{}
+	extractor := NewParamExtractor()
+	err = extractor.ExtractForms(&empty, req)
+	if err != nil {
+		t.Errorf(`error extracting forms due to %v`, err)
+	}
+	if empty.Name != `` {
+		t.Errorf(`expected "", but received %v`, empty.Name)
+	}
+	if empty.Age != 0 {
+		t.Errorf(`expected 0, but received %v`, empty.Age)
+	}
+	if empty.Height != 0 {
+		t.Errorf(`expected 0, but received %v`, empty.Height)
+	}
+}
+
+func TestExtractor_ExtractQueries_EmptyFieldTag(t *testing.T) {
+	req, err := makeRequest()
+	if err != nil {
+		t.Error(`error creating request`, err)
+	}
+
+	empty := emptyTag{}
+	extractor := NewParamExtractor()
+	err = extractor.ExtractQueries(&empty, req)
+	if err != nil {
+		t.Errorf(`error extracting quaries due to %v`, err)
+	}
+	if empty.Name != `` {
+		t.Errorf(`expected "", but received %v`, empty.Name)
+	}
+	if empty.Age != 0 {
+		t.Errorf(`expected 0, but received %v`, empty.Age)
+	}
+	if empty.Height != 0 {
+		t.Errorf(`expected 0, but received %v`, empty.Height)
+	}
+}
+
+func TestExtractor_Errors(t *testing.T) {
+	req, err := makeRequest()
+	if err != nil {
+		t.Error(`error creating request`, err)
+	}
+	extractor := NewParamExtractor()
+
+	t.Run(`test not assignable error`, func(t *testing.T) {
+		header := headerParams{}
+		err := extractor.ExtractHeaders(header, req)
+		_, ok := err.(ErrorNotAssignable)
+		if !ok {
+			t.Errorf(`expected "ErrorNotAssignable", but received %v`, reflect.TypeOf(err))
+			t.Fail()
+		}
+	})
+
+	t.Run(`test non struct type error`, func(t *testing.T) {
+		i := int64(0)
+		err := extractor.ExtractHeaders(&i, req)
+		_, ok := err.(ErrorUnSupportedType)
+		if !ok {
+			t.Errorf(`expected "ErrorUnSupportedType", but received %v`, reflect.TypeOf(err))
+		}
+	})
+
+	t.Run(`test unsupported param type error`, func(t *testing.T) {
+		obj := unSupportedTypeError{}
+		err := extractor.ExtractHeaders(&obj, req)
+		_, ok := err.(ErrorUnSupportedParamType)
+		if !ok {
+			t.Errorf(`expected "ErrorUnSupportedParamType", but received %v`, reflect.TypeOf(err))
+			t.Fail()
+		}
+	})
+
+	t.Run(`test unmarshal type error "bool"`, func(t *testing.T) {
+		obj := boolError{}
+		err := extractor.ExtractHeaders(&obj, req)
+		if err == nil {
+			t.Errorf(`expected "ErrorUnmarshalType", but received "%v"`, nil)
+			return
+		}
+		_, ok := err.(ErrorUnmarshalType)
+		if !ok {
+			t.Errorf(`expected "ErrorUnmarshalType", but received "%v"`, reflect.TypeOf(err))
+		}
+		exErr := `error unmarshalling [header_name] into [bool] due to strconv.ParseBool: parsing "header_name": invalid syntax`
+		if err.Error() != exErr {
+			t.Errorf(`expexted [%v], but received [%v]`, exErr, err.Error())
+		}
+	})
+
+	t.Run(`test unmarshal type error "int32"`, func(t *testing.T) {
+		obj := int32Error{}
+		err := extractor.ExtractHeaders(&obj, req)
+		if err == nil {
+			t.Errorf(`expected "ErrorUnmarshalType", but received %v`, nil)
+			return
+		}
+		_, ok := err.(ErrorUnmarshalType)
+		if !ok {
+			t.Errorf(`expected "ErrorUnmarshalType", but received %v`, reflect.TypeOf(err))
+		}
+		exErr := `error unmarshalling [header_name] into [int32] due to strconv.Atoi: parsing "header_name": invalid syntax`
+		if err.Error() != exErr {
+			t.Errorf(`expexted [%v], but received [%v]`, exErr, err.Error())
+		}
+	})
+
+	t.Run(`test unmarshal type error "int"`, func(t *testing.T) {
+		obj := intError{}
+		err := extractor.ExtractHeaders(&obj, req)
+		if err == nil {
+			t.Errorf(`expected "ErrorUnmarshalType", but received %v`, nil)
+			return
+		}
+		_, ok := err.(ErrorUnmarshalType)
+		if !ok {
+			t.Errorf(`expected "ErrorUnmarshalType", but received %v`, reflect.TypeOf(err))
+		}
+		exErr := `error unmarshalling [header_name] into [int] due to strconv.Atoi: parsing "header_name": invalid syntax`
+		if err.Error() != exErr {
+			t.Errorf(`expexted [%v], but received [%v]`, exErr, err.Error())
+		}
+	})
+}
+
 type headerParams struct {
 	Name    string  `param:"name"`
 	Age     int64   `param:"age"`
@@ -80,4 +247,38 @@ type queryParams struct {
 	Age     int32   `param:"age"`
 	Height  float32 `param:"height"`
 	Married bool    `param:"married"`
+}
+
+type emptyTag struct {
+	Name   string  `param:"-"`
+	Age    int32   `param:""`
+	Height float32 `param:"test"`
+}
+
+type boolError struct {
+	Name bool `param:"name"`
+}
+
+type int32Error struct {
+	Name int32 `param:"name"`
+}
+
+type intError struct {
+	Name int `param:"name"`
+}
+
+type int64Error struct {
+	Name int64 `param:"name"`
+}
+
+type float32Error struct {
+	Name float32 `param:"name"`
+}
+
+type float64Error struct {
+	Name float64 `param:"name"`
+}
+
+type unSupportedTypeError struct {
+	Name map[string]string `param:"name"`
 }
