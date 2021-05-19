b91981a5d8254a8eed9b026dfe8a65dd05926744
nipunapathirana <nsspathirana@gmail.com>
2021-05-20 00:59:15 +0530

    comments added

--- error.go
+++ error.go
@@ -1,23 +1,27 @@
 package paramex
 
+// ErrorUnSupportedParamType created when trying to extract unsupported parameter type
 type ErrorUnSupportedParamType struct{ err error }
 
 func (e ErrorUnSupportedParamType) Error() string {
 	return e.err.Error()
 }
 
+// ErrorUnmarshalType created when trying to marshal different type value to another type variable
 type ErrorUnmarshalType struct{ err error }
 
 func (e ErrorUnmarshalType) Error() string {
 	return e.err.Error()
 }
 
+// ErrorNotAssignable created when sent interface to extract  is not assignable. Not a reference to a Go struct
 type ErrorNotAssignable struct{ err error }
 
 func (e ErrorNotAssignable) Error() string {
 	return e.err.Error()
 }
 
+// ErrorUnSupportedType created when sent reference  is not a Go struct type reference
 type ErrorUnSupportedType struct{ err error }
 
 func (e ErrorUnSupportedType) Error() string {
