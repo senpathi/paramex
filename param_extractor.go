89cddc6aadc8c92f5afb4e292623c1545f5d1cf4
nipunapathirana <nsspathirana@gmail.com>
2021-08-25 21:30:14 +0530

    []string type support added

--- param_extractor.go
+++ param_extractor.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/uuid"
 )
 
-type extractorFunc func(key string) (string, bool)
+type extractorFunc func(key string, array bool) (interface{}, bool)
 
 const (
 	stringType  = string("")
@@ -49,7 +49,10 @@ func NewParamExtractor() Extractor {
 
 // ExtractHeaders extract http headers from sent request and binds to v
 func (p extractor) ExtractHeaders(v interface{}, req *http.Request) error {
-	return p.extract(v, func(key string) (string, bool) {
+	return p.extract(v, func(key string, array bool) (interface{}, bool) {
+		if array {
+			return nil, false
+		}
 		str := req.Header.Get(key)
 		if str == `` {
 			return str, false
@@ -60,10 +63,14 @@ func (p extractor) ExtractHeaders(v interface{}, req *http.Request) error {
 
 // ExtractQueries extract http url parameters from sent request and binds to v
 func (p extractor) ExtractQueries(v interface{}, req *http.Request) error {
-	return p.extract(v, func(key string) (string, bool) {
-		str := req.URL.Query().Get(key)
-		if str == `` {
-			return str, false
+	return p.extract(v, func(key string, array bool) (interface{}, bool) {
+		str := req.URL.Query()[key]
+		if len(str) == 0 {
+			return "", false
+		}
+
+		if !array {
+			return str[0], true
 		}
 		return str, true
 	})
@@ -71,10 +78,18 @@ func (p extractor) ExtractQueries(v interface{}, req *http.Request) error {
 
 // ExtractForms extract http form values from sent request and binds to v
 func (p extractor) ExtractForms(v interface{}, req *http.Request) error {
-	return p.extract(v, func(key string) (string, bool) {
-		str := req.FormValue(key)
-		if str == `` {
-			return str, false
+	err := req.ParseForm()
+	if err != nil {
+		return err
+	}
+	return p.extract(v, func(key string, array bool) (interface{}, bool) {
+		str := req.PostForm[key]
+		if len(str) == 0 {
+			return "", false
+		}
+
+		if !array {
+			return str[0], true
 		}
 		return str, true
 	})
@@ -102,17 +117,19 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			continue
 		}
 
-		valueStr, ok := keyExtractor(tag)
+		_, ok = keyExtractor(tag, false)
 		if !ok {
 			continue
 		}
 
 		switch field.Type {
 		case reflect.TypeOf(stringType):
-			elem.Field(i).Set(reflect.ValueOf(valueStr))
+			valueStr, _ := keyExtractor(tag, false)
+			elem.Field(i).Set(reflect.ValueOf(valueStr.(string)))
 
 		case reflect.TypeOf(boolType):
-			value, err := strconv.ParseBool(valueStr)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := strconv.ParseBool(valueStr.(string))
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [bool] due to %v`, valueStr, err)}
@@ -120,7 +137,8 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			elem.Field(i).Set(reflect.ValueOf(value))
 
 		case reflect.TypeOf(int32Type):
-			value, err := strconv.Atoi(valueStr)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := strconv.Atoi(valueStr.(string))
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [int32] due to %v`, valueStr, err)}
@@ -128,7 +146,8 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			elem.Field(i).Set(reflect.ValueOf(int32(value)))
 
 		case reflect.TypeOf(intType):
-			value, err := strconv.Atoi(valueStr)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := strconv.Atoi(valueStr.(string))
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [int] due to %v`, valueStr, err)}
@@ -136,7 +155,8 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			elem.Field(i).Set(reflect.ValueOf(value))
 
 		case reflect.TypeOf(int64Type):
-			value, err := strconv.ParseInt(valueStr, 10, 64)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := strconv.ParseInt(valueStr.(string), 10, 64)
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [int64] due to %v`, valueStr, err)}
@@ -144,7 +164,8 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			elem.Field(i).Set(reflect.ValueOf(value))
 
 		case reflect.TypeOf(float32Type):
-			value, err := strconv.ParseFloat(valueStr, 32)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := strconv.ParseFloat(valueStr.(string), 32)
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [float32] due to %v`, valueStr, err)}
@@ -152,7 +173,8 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			elem.Field(i).Set(reflect.ValueOf(float32(value)))
 
 		case reflect.TypeOf(float64Type):
-			value, err := strconv.ParseFloat(valueStr, 64)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := strconv.ParseFloat(valueStr.(string), 64)
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [float64] due to %v`, valueStr, err)}
@@ -160,12 +182,22 @@ func (p extractor) extract(v interface{}, keyExtractor extractorFunc) error {
 			elem.Field(i).Set(reflect.ValueOf(value))
 
 		case reflect.TypeOf(uuid.UUID{}):
-			value, err := uuid.Parse(valueStr)
+			valueStr, _ := keyExtractor(tag, false)
+			value, err := uuid.Parse(valueStr.(string))
 			if err != nil {
 				return ErrorUnmarshalType{
 					fmt.Errorf(`error unmarshalling [%v] into [uuid] due to %v`, valueStr, err)}
 			}
 			elem.Field(i).Set(reflect.ValueOf(value))
+
+		case reflect.TypeOf([]string{stringType}):
+			valueStr, ok := keyExtractor(tag, true)
+			if !ok {
+				return ErrorUnSupportedParamType{
+					fmt.Errorf(`error unmarshalling []string into "%v", unsupported param type`, tag),
+				}
+			}
+			elem.Field(i).Set(reflect.ValueOf(valueStr.([]string)))
 
 		default:
 			return ErrorUnSupportedParamType{errors.New(`unsupported param extractor type`)}
